a85f15c53e55b9c76d6586f4cdfdbe58d239f791
victoria-w-h <victoria@stormcreative.co.uk>
2024-02-29 09:24:25 +0000

    settings toggles and local storage

--- form.js
+++ form.js
@@ -1,3 +1,5 @@
+// message user form
+
 const input = document.getElementById("user-search-input");
 const textarea = document.getElementById("user-message-textarea");
 const messageUserForm = document.getElementById("message-user-form");
@@ -20,3 +22,52 @@ messageUserForm.addEventListener("submit", (e) => {
 		swal("Please fill in all fields", "", "error");
 	}
 });
+
+// settings
+
+const emailSwitchCheckbox = document.getElementById("email-switch-check");
+const profileSwitchCheckbox = document.getElementById("profile-switch-check");
+
+const settingSave = document.getElementById("setting-save");
+
+window.onload = function () {
+	const emailStorageOn = localStorage.getItem("Email Preference") === "on";
+	const profileStorageOn =
+		localStorage.getItem("Profile Preference") === "on";
+
+	if (emailStorageOn && profileStorageOn) {
+		emailSwitchCheckbox.checked = true;
+		profileSwitchCheckbox.checked = true;
+	} else if (emailStorageOn && !profileStorageOn) {
+		emailSwitchCheckbox.checked = true;
+		profileSwitchCheckbox.checked = false;
+	} else if (!emailStorageOn && profileStorageOn) {
+		emailSwitchCheckbox.checked = false;
+		profileSwitchCheckbox.checked = true;
+	} else {
+		emailSwitchCheckbox.checked = false;
+		profileSwitchCheckbox.checked = false;
+	}
+};
+
+function checkEmailPreference() {
+	if (emailSwitchCheckbox.checked) {
+		console.log("email preference is on");
+		localStorage.setItem("Email Preference", "on");
+	} else {
+		console.log("email preference is off");
+		localStorage.setItem("Email Preference", "off");
+	}
+}
+
+function checkProfilePreference() {
+	if (profileSwitchCheckbox.checked) {
+		console.log("profile preference is on");
+		localStorage.setItem("Profile Preference", "on");
+	} else {
+		console.log("Profile preference is off");
+		localStorage.setItem("Profile Preference", "off");
+	}
+}
+settingSave.addEventListener("click", checkEmailPreference);
+settingSave.addEventListener("click", checkProfilePreference);
